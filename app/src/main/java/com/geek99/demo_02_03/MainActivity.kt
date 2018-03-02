290a1df8730dbf8283c06361ca66c9812e9353e7
gaishengwen <930985649@qq.com>
2018-03-02 11:00:32 +0800

    Initial commit

---
+++ app/src/main/java/com/geek99/demo_02_03/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.geek99.demo_02_03
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
